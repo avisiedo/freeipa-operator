654c2818cca761a8c01eb62d811b4a0629790792
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-03-03 17:03:49 +0100

    fix: make operator-sdk
    - syntax error when closing the case block statement 'esac ;' instead of
      'esac) ;'
    - Add a new line at the last line.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- mk/miscelanea.mk
+++ mk/miscelanea.mk
@@ -13,7 +13,7 @@ OPERATOR_SDK := $(PWD)/bin/operator-sdk
 operator-sdk: $(PROJECT_DIR)/bin ## Donwload operator-sdk
 ifeq (,$(wildcard $(OPERATOR_SDK)))
 	set -e ; \
-	export ARCH=$$(case $$(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $$(uname -m) ;; esac) ; \
+	export ARCH=$$(case $$(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $$(uname -m) ;; esac ; \
 	export OS=$$(uname | awk '{print tolower($$0)}') ; \
 	export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.18.0 ; \
 	curl -sSLo "$(OPERATOR_SDK)" "$$OPERATOR_SDK_DL_URL/operator-sdk_$${OS}_$${ARCH}" && \
